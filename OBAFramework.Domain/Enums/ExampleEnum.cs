5f8d252be7d5a763fb80027f6ade87d7794e3e8f
Farhad <farxadjr@gmail.com>
2025-06-12 12:10:47 +0400

    enum added

---
+++ OBAFramework.Domain/Enums/ExampleEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBAFramework.Domain.Enums
+{
+    public enum ExampleEnum
+    {
+        January,
+        Febuary,
+        March,
+        April
+    }
+}
